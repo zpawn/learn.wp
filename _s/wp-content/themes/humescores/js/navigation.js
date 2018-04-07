8332107e4a8266fdf8de8248c849409386f1662f
zpawn <ilia.makarov@me.com>
2018-04-07 19:07:46 +0300

    _s: Customize JavaScript to work with Current Theme

--- _s/wp-content/themes/humescores/js/navigation.js
+++ _s/wp-content/themes/humescores/js/navigation.js
@@ -12,23 +12,17 @@
 
         // Add dropdown toggle that displays child menu items.
         var dropdownToggle = $( '<button />', { 'class': 'dropdown-toggle', 'aria-expanded': false })
-            .append( humescoresScreenReaderText.icon )
+            .append( $( '<span />', { 'class': 'dropdown-symbol', text: '+' }) )
             .append( $( '<span />', { 'class': 'screen-reader-text', text: humescoresScreenReaderText.expand }) );
 
         container.find( '.menu-item-has-children > a, .page_item_has_children > a' ).after( dropdownToggle );
 
-        // Set the active submenu dropdown toggle button initial state.
-        container.find( '.current-menu-ancestor > button' )
-            .addClass( 'toggled-on' )
-            .attr( 'aria-expanded', 'true' )
-            .find( '.screen-reader-text' )
-            .text( humescoresScreenReaderText.collapse );
-        // Set the active submenu initial state.
-        container.find( '.current-menu-ancestor > .sub-menu' ).addClass( 'toggled-on' );
-
         container.find( '.dropdown-toggle' ).click( function( e ) {
             var _this = $( this ),
-                screenReaderSpan = _this.find( '.screen-reader-text' );
+                screenReaderSpan = _this.find( '.screen-reader-text' ),
+				dropdownSymbol = _this.find('.dropdown-symbol');
+
+            dropdownSymbol.text( dropdownSymbol.text() === '-' ? '+' : '-' );
 
             e.preventDefault();
             _this.toggleClass( 'toggled-on' );
